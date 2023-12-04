614e99dd4089e01f5174bc71c92d0ae31e8aa5b8
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2023-12-03 21:57:39 -0300

    feat: Relacionsmento entre preço e curso

--- Data/Curso.cs
+++ Data/Curso.cs
@@ -6,7 +6,7 @@ public class Curso
     public string? Titulo{get; set;}
     public string? Descricao{get; set;}
     public System.DateTime? DataDePublicacao{get; set;}
-    
-    // public byte? FotoDeCapa{get; set;}
+    public byte[]? FotoDeCapa{get; set;}
+    public Preco? PrecoPromocao{get; set;}
 }
 
--- Data/Preco.cs
+++ Data/Preco.cs
@@ -5,6 +5,10 @@ public class Preco
     public int PrecoId { get; set; }
     public decimal PrecoAtual { get; set; }
     public decimal Promocao { get; set; }
+
     public int CursoId { get; set; }
+
+    //Relação de Preços e Cursos
+    public Curso? Curso { get; set; }
 }
 
